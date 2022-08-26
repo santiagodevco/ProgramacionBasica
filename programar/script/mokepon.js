50c1ea93fbf9eb374ffee901d07d9ce6f68e97ee
Santiago Murcia <santiagomurcia.2100@gmail.com>
2022-08-25 21:27:37 -0500

    Se alistaron los ataques del jugador

--- programar/script/mokepon.js
+++ programar/script/mokepon.js
@@ -1,7 +1,18 @@
+let ataqueJugador
+
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById('boton-mascota')
 
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.addEventListener('click', ataqueAgua)
+
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click', ataqueTierra)
 }
 
 function seleccionarMascotaJugador(){
@@ -22,6 +33,42 @@ function seleccionarMascotaJugador(){
     else{
         alert('No has seleccionado tu personaje')
     }
+
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if (ataqueAleatorio == 1 ){
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    }
+    else if(ataqueAleatorio == 2){
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    }
+    else{
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+}
+
+function ataqueFuego(){
+    ataqueJugador = 'Fuego'
+    alert(ataqueJugador)
+}
+
+function ataqueAgua(){
+    ataqueJugador = 'Agua'
+    alert(ataqueJugador)
+}
+
+function ataqueTierra(){
+    ataqueJugador =  'Tierra'
+    alert(ataqueJugador)
+}
+
+function aleatorio(min, max){
+    return Math.floor(Math.random()* (max - min + 1) + min)
 }
 
 window.addEventListener('load', iniciarJuego)
