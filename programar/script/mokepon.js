4f26f52382aaef436f9784da63a3c2952284d4d8
Santiago Murcia <santiagomurcia.2100@gmail.com>
2022-09-14 09:14:24 -0500

    Se realizo la optimizacion del frontend

--- programar/script/mokepon.js
+++ programar/script/mokepon.js
@@ -163,14 +163,17 @@ function secuenciaAtaque(){
                 ataqueJugador.push('FUEGO')
                 console.log(ataqueJugador)
                 boton.style.background = '#112f58'
+                boton.disabled = true
             }else if (e.target.textContent === '💧'){
                 ataqueJugador.push('AGUA')
                 console.log(ataqueJugador)
                 boton.style.background = '#112f58'
+                boton.disabled = true
             }else {
                 ataqueJugador.push('TIERRA')
                 console.log(ataqueJugador)
                 boton.style.background = '#112f58'
+                boton.disabled = true
             }
             ataqueAleatorioEnemigo()
         })
@@ -270,9 +273,7 @@ function crearMensaje(resultado) {
 function crearMensajeFinal(resultadoFinal) {
     
     sectionMensajes.innerHTML = resultadoFinal
-    botonFuego.disabled = true
-    botonAgua.disabled = true
-    botonTierra.disabled = true
+    
     sectionReiniciar.style.display = 'block'
 }
 
